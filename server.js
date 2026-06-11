403acc2fe7f695b992e790bbbe85b48226c64274
dssl1-0502.github.io <dssl1@aluno.ifal.edu.br>
2026-06-11 07:24:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express();
 // TODO: implemente a rota GET /
 // A resposta deve conter a palavra "Hello"
 app.get('/', (req, res) => {
-  res.send('Hello');
+  res.send('Hello, world');
 });
 
 app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000');
 });
 
-Débora fez um commit
+
